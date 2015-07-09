bf4900b3a05564ca939434dfd023731951f81745
Dotosoft <dotosoft@gmail.com>
2015-07-10 02:25:45 +0700

    update port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // app.listen(80);
 
-var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8000;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 app.listen(port, address);
